030dda6314b740b0f2692ae8e1cfd30d962149fe
robertoDCJ <robertox70@hotmail.com>
2024-06-16 11:36:58 -0300

    Created validationContext middleware

---
+++ front/src/middleware/validationContext.ts
@@ -0,0 +1,19 @@
+import { useContextForm } from "@/context/Context";
+import { useRouter } from "next/navigation";
+
+export const validationContext = () => {
+  const { nomeCompleto, telefone, email, cepHospital, cargo } =
+    useContextForm();
+
+  const route = useRouter();
+
+  if (
+    nomeCompleto.length === 0 &&
+    telefone.length === 0 &&
+    email.length === 0 &&
+    cepHospital.length === 0 &&
+    cargo.length === 0
+  ) {
+    route.push("/formulario");
+  }
+};

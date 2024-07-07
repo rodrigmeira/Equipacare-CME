26b3fc09a0343f3ca68a671ac52691f6e7a1e594
robertoDCJ <robertox70@hotmail.com>
2024-07-06 23:08:38 -0300

    created EachInputRadio

--- front/src/components/Step3/InputRadio.tsx
+++ front/src/components/Step3/InputRadio.tsx
@@ -1,3 +1,6 @@
+import { EachInputRadio } from "./EachInputRadio";
+import { H3 } from "./H3";
+
 type InputRadioProps = {
   title: string;
   firstInput: string;
@@ -16,31 +19,15 @@ export const InputRadio = ({
   name,
 }: InputRadioProps) => {
   return (
-    <div className="font-extralight">
-      <h3 className="mb-2 text-base select-none">{title}</h3>
+    <div className="font-extralight select-none">
+      <H3>{title}</H3>
       <div className="flex flex-row gap-5">
-        <div className="flex justify-center items-center">
-          <input
-            className="size-4 appearance-none border border-black rounded-full before:content-[''] before:block before:size-[10px] before:translate-y-[2.3px] before:bg-white before:rounded-full before:m-auto before:transition-colors before:duration-300 before:ease-in-out checked:before:bg-second select-none"
-            type="radio"
-            id={firstInput}
-            name={name}
-          />
-          <label className="ml-2 text-sm select-none" htmlFor={firstInput}>
-            {firstChildren}
-          </label>
-        </div>
-        <div className="flex justify-center items-center">
-          <input
-            className="size-4 appearance-none border border-black rounded-full before:content-[''] before:block before:size-[10px] before:translate-y-[2.3px] before:bg-white before:rounded-full before:m-auto before:transition-colors before:duration-300 before:ease-in-out checked:before:bg-second  select-none"
-            type="radio"
-            id={secondInput}
-            name={name}
-          />
-          <label className="ml-2 text-sm select-none" htmlFor={secondInput}>
-            {secondChildren}
-          </label>
-        </div>
+        <EachInputRadio id={firstInput} name={name}>
+          {firstChildren}
+        </EachInputRadio>
+        <EachInputRadio id={secondInput} name={name}>
+          {secondChildren}
+        </EachInputRadio>
       </div>
     </div>
   );
--- front/src/components/Step3/Step3.tsx
+++ front/src/components/Step3/Step3.tsx
@@ -1,3 +1,4 @@
+import { H3 } from "./H3";
 import { InputRadio } from "./InputRadio";
 
 export const Step3 = () => {
@@ -19,6 +20,116 @@ export const Step3 = () => {
         secondChildren="Substituir"
         name="ampliarSubstituir"
       />
+      <div className="select-none font-extralight">
+        <H3>As cirurgias serão realizadas em quais dias da semana?</H3>
+        <div className="flex flex-row gap-4">
+          <div className="flex flex-col">
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="todosOsDias"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="todosOsDias"
+                id="todosOsDias"
+              />
+              <span className="ml-2 text-sm">Todos os dias</span>
+            </label>
+
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="segundaFeira"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="segundaFeira"
+                id="segundaFeira"
+              />
+              <span className="ml-2 text-sm">Segunda-feira</span>
+            </label>
+          </div>
+          <div className="flex flex-col">
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="tercaFeira"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="tercaFeira"
+                id="tercaFeira"
+              />
+              <span className="ml-2 text-sm">Terça-feira</span>
+            </label>
+
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="quartaFeira"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="quartaFeira"
+                id="quartaFeira"
+              />
+              <span className="ml-2 text-sm">Quarta-feira</span>
+            </label>
+          </div>
+          <div className="flex flex-col">
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="quintaFeira"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="quintaFeira"
+                id="quintaFeira"
+              />
+              <span className="ml-2 text-sm">Quinta-feira</span>
+            </label>
+
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="sextaFeira"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="sextaFeira"
+                id="sextaFeira"
+              />
+              <span className="ml-2 text-sm">Sexta-feira</span>
+            </label>
+          </div>
+          <div className="flex flex-col">
+            <label className="relative flex items-center mb-2" htmlFor="Sabado">
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="Sabado"
+                id="Sabado"
+              />
+              <span className="ml-2 text-sm">Sabado</span>
+            </label>
+
+            <label
+              className="relative flex items-center mb-2"
+              htmlFor="domingo"
+            >
+              <input
+                className="appearance-none size-4 border border-gray-300 rounded before:content-[''] before:block before:w-full before:h-full before:bg-white before:rounded checked:before:bg-second checked:after:content-['✔'] checked:after:absolute checked:after:top-0 checked:after:-left-12 checked:after:w-full checked:after:h-full checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-white"
+                type="checkbox"
+                name="domingo"
+                id="domingo"
+              />
+              <span className="ml-2 text-sm">Domingo</span>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

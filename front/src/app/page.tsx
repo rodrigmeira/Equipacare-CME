1b130ffa1303494e137d2cf8f3020ba9b9998170
Rodrigo Brandão <meiramusic@Outlook.com>
2024-07-07 20:34:32 -0300

    feat: previous step add + button adjustment

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,6 +4,8 @@ import { ButtonForm, Progress, Step1, Step3 } from "@/components";
 import { Step2 } from "@/components/Step2/Step2";
 import { Step4 } from "@/components/Step4/Step4";
 import { FormEvent, useEffect, useState } from "react";
+import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Page() {
   const [count, setCount] = useState(1);
@@ -33,8 +35,14 @@ export default function Page() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (count < 3) {
+    if (count < 4) {
       setCount(count + 1);
+    }
+  };
+
+  const handleGoBackToPreviousStep = () => {
+    if (count > 1) {
+      setCount(count - 1);
     }
   };
 
@@ -44,7 +52,16 @@ export default function Page() {
         value={progress}
         className="absolute top-0 left-0 w-full h-4 border-1 rounded-none"
       />
-      <h3 className="text-base font-extralight">Step {count} of 4</h3>
+      <div className="flex flex-row gap-2 items-center">
+        <button onClick={handleGoBackToPreviousStep} type="button" className={count > 1 ? "pointer" : "pointer hidden"}>
+          <FontAwesomeIcon
+            icon={faCircleArrowLeft}
+            style={{ color: "#8edd2a" }}
+          />
+        </button>
+
+        <h3 className="text-base font-extralight">Step {count} of 4</h3>
+      </div>
       {count <= 3 && (
         <h1 className="text-2xl font-bold">Preencha as informações</h1>
       )}
@@ -54,7 +71,7 @@ export default function Page() {
         {count === 2 && <Step2 />}
         {count === 3 && <Step3 />}
         {count === 4 && <Step4 />}
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center absolute bottom-0 left-0 right-0 mb-16">
           {count < 4 && <ButtonForm>Próximo</ButtonForm>}
           {count === 4 && null}
         </div>

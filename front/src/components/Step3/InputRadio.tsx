9cff58a6b209ce37308a9b7f1f312df265a10970
robertoDCJ <robertox70@hotmail.com>
2024-07-06 22:05:25 -0300

    created InputRadio

--- front/src/components/Step3/InputRadio.tsx
+++ front/src/components/Step3/InputRadio.tsx
@@ -17,17 +17,27 @@ export const InputRadio = ({
 }: InputRadioProps) => {
   return (
     <div className="font-extralight">
-      <h3 className="mb-2 text-base">{title}</h3>
+      <h3 className="mb-2 text-base select-none">{title}</h3>
       <div className="flex flex-row gap-5">
         <div className="flex justify-center items-center">
-          <input className="size-4" type="radio" id={firstInput} name={name} />
-          <label className="ml-2 text-sm" htmlFor={firstInput}>
+          <input
+            className="size-4 appearance-none border border-black rounded-full before:content-[''] before:block before:size-[10px] before:translate-y-[2.3px] before:bg-white before:rounded-full before:m-auto before:transition-colors before:duration-300 before:ease-in-out checked:before:bg-second select-none"
+            type="radio"
+            id={firstInput}
+            name={name}
+          />
+          <label className="ml-2 text-sm select-none" htmlFor={firstInput}>
             {firstChildren}
           </label>
         </div>
         <div className="flex justify-center items-center">
-          <input className="size-4" type="radio" id={secondInput} name={name} />
-          <label className="ml-2 text-sm" htmlFor={secondInput}>
+          <input
+            className="size-4 appearance-none border border-black rounded-full before:content-[''] before:block before:size-[10px] before:translate-y-[2.3px] before:bg-white before:rounded-full before:m-auto before:transition-colors before:duration-300 before:ease-in-out checked:before:bg-second  select-none"
+            type="radio"
+            id={secondInput}
+            name={name}
+          />
+          <label className="ml-2 text-sm select-none" htmlFor={secondInput}>
             {secondChildren}
           </label>
         </div>

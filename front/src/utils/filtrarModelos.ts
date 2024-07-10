9bba1b7ba4926a1fb66993b697e402a828cbdffb
robertoDCJ <robertox70@hotmail.com>
2024-07-10 17:40:00 -0300

    Fixing error in the filtrarModelos

--- front/src/utils/filtrarModelos.ts
+++ front/src/utils/filtrarModelos.ts
@@ -9,5 +9,15 @@ export function filtrarModelos(modelos: filtrarModelosProps[], marca: string) {
     .filter((item: filtrarModelosProps) => item.marca.includes(marca))
     .sort((a, b) => a.preco - b.preco);
 
+  if (modelo.length === 0) {
+    return [
+      {
+        marca: marca,
+        modelo: "Indisponível",
+        preco: "Indisponível",
+      },
+    ];
+  }
+
   return modelo;
 }

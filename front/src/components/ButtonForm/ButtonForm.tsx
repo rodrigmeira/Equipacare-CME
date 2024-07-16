80555e3c1b31c2d2e7976b2beed16689f3382a30
Rodrigo Brandão <meiramusic@Outlook.com>
2024-07-16 01:06:20 -0300

    att: disable button color

--- front/src/components/ButtonForm/ButtonForm.tsx
+++ front/src/components/ButtonForm/ButtonForm.tsx
@@ -12,7 +12,7 @@ export const ButtonForm = ({
     <button
       type="submit"
       className={`border font-semibold tracking-widest text-base py-2 px-20 rounded-lg select-none ${
-        disabled ? "bg-gray-400 cursor-not-allowed" : "bg-prima hover:bg-second text-white"
+        disabled ? "bg-gray-200 cursor-not-allowed" : "bg-prima hover:bg-second text-white"
       } ${className}`}
       disabled={disabled}
       {...props}
